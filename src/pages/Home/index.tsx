7bacdacbfb23a4f16224a1630a41301e6ac19514
Kirill Shchipanov <shchipanovk@mail.ru>
2024-03-27 04:31:03 +0300

    working search

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {fetchPosts, setItem} from "../../redux/slices/postSlice";
+import {fetchPosts} from "../../redux/slices/postSlice";
 
 import styles from './Home.module.scss'
 import {RootState, useAppDispatch} from "../../redux/store";
@@ -11,22 +11,13 @@ const Home: React.FC = () => {
   const dispatch = useAppDispatch()
   const {allPosts} = useSelector((state: RootState) => state.posts)
   const [search, setSearch] = useState('')
+  const filtered = allPosts.filter((post) => post.title.includes(search))
 
   const getPosts = async () => dispatch(fetchPosts('https://jsonplaceholder.typicode.com/posts?_limit=5'))
 
   useEffect(() => {
     if (allPosts.length === 0 && search===''){
       getPosts()
-      console.log(allPosts)
-    }
-  }, [search]);
-
-  useEffect(() => {
-    if (search !== '') {
-      getPosts().then(data => {
-        console.log(data.payload)
-      })
-      dispatch(setItem(allPosts.filter((post) => post.title.includes(search))))
     }
   }, [search]);
 
@@ -35,12 +26,12 @@ const Home: React.FC = () => {
       <h1>Блог</h1>
       <p>Здесь мы делимся интересными кейсами из наших проектов, пишем про IT, а также переводим зарубежные статьи</p>
       <Search setSearch={setSearch}/>
-      {allPosts.length && (<div className={styles.all_posts}>
-        {<Post first={true} {...allPosts[0]}/>}
+      {filtered.length ? (<div className={styles.all_posts}>
+        {<Post first={true} {...filtered[0]}/>}
         <div className={styles.posts}>
-          {allPosts.slice(1).map((post) => <Post key={post.id} {...post}/>)}
+          {filtered.slice(1).map((post) => <Post key={post.id} {...post}/>)}
         </div>
-      </div>) }
+      </div>) : <></>}
     </div>
   )
 }
